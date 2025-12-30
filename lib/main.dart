d5f7fc6746fea3524b5a540ce594ca4b217d0602
paisul <paisul.paree@elegantmedia.lk>
2025-12-30 10:19:57 +0530

    Refactor main.dart to enhance theme configuration with dark mode support and improved styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,59 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
-  runApp(MaterialApp(theme: ThemeData(useMaterial3: true), home: Expenses()));
+  runApp(
+    MaterialApp(
+      darkTheme:
+          ThemeData.from(
+            colorScheme: kDarkColorScheme,
+            useMaterial3: true,
+          ).copyWith(
+            cardTheme: const CardThemeData().copyWith(
+              color: kDarkColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: kDarkColorScheme.primaryContainer,
+                backgroundColor: kDarkColorScheme.onPrimaryContainer,
+              ),
+            ),
+          ),
+      theme: ThemeData.from(useMaterial3: true, colorScheme: kColorScheme)
+          .copyWith(
+            appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            ),
+            cardTheme: const CardThemeData().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer,
+              ),
+            ),
+            textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+          ),
+      // themeMode: ThemeMode.system, // default
+      home: Expenses(),
+    ),
+  );
 }
